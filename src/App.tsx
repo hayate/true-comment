9a29c95fb6b4caa069c17f59c46f399705c0a0dc
hayate <andrea@byteset.com>
2023-02-05 15:33:05 +0900

    8 years later...

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+  const [url, setUrl] = useState<string | undefined>('');
+
+  useEffect(() => {
+    const queryInfo = {active: true, lastFocusedWindow: true};
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const currentURL = tabs[0].url;
+      setUrl(currentURL);
+    });
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{url}</p>
       </header>
     </div>
   );
